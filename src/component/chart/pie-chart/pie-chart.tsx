655d2621fcb5e67fc527f61374f229ea0affc9ce
AnushervonKhidirov <ankhid98@gmail.com>
2024-08-08 19:31:30 +0500

    pie chart updated

--- src/component/chart/pie-chart/pie-chart.tsx
+++ src/component/chart/pie-chart/pie-chart.tsx
@@ -1,25 +1,47 @@
 'use client'
 import { PieChart } from '@mui/x-charts'
-import type { PieItemId, PieValueType } from '@mui/x-charts'
+import type { PieItemId } from '@mui/x-charts'
 
 const pieParams = { height: 200 }
 
-export type TPieChart = {
+export type TPieChartData = {
+    id: PieItemId
+    value: number
+    label: string
+}
+
+type TPieChartDataPercent = {
     id: PieItemId
     value: number
     label: string
     percent: string
 }
 
-const Chart = ({ data }: { data: TPieChart[] }) => {
+const Chart = ({ data }: { data: TPieChartData[] }) => {
+    const updatedData = calcPercent(data)
+
+    function calcPercent(data: TPieChartData[]): TPieChartDataPercent[] {
+        let totalValue = 0
+        let newData = data as TPieChartDataPercent[]
+    
+        newData.forEach(item => {
+            totalValue += item.value
+        })
+    
+        return newData.map(item => {
+            item.percent = (item.value / (totalValue / 100)).toFixed(2) + '%'
+            return item
+        })
+    }
+
     return (
         <PieChart
             series={[
                 {
-                    data: data,
+                    data: updatedData,
                     arcLabel: item => `${item.label}`,
                     valueFormatter: (_, { dataIndex }) => {
-                        return `${data[dataIndex].value} users (${data[dataIndex].percent})`
+                        return `${updatedData[dataIndex].value} users (${updatedData[dataIndex].percent})`
                     },
                 },
             ]}
@@ -29,20 +51,6 @@ const Chart = ({ data }: { data: TPieChart[] }) => {
             {...pieParams}
         ></PieChart>
     )
-}
-
-export function calcPercent(data: PieValueType[]): TPieChart[] {
-    let totalValue = 0
-    let newData = data as TPieChart[]
-
-    newData.forEach(item => {
-        totalValue += item.value
-    })
-
-    return newData.map(item => {
-        item.percent = (item.value / (totalValue / 100)).toFixed(2) + '%'
-        return item
-    })
 }
 
 export default Chart
