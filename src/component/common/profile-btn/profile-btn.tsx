62a87ec5781b736424514c798618e0e9d2d5e7ab
AnushervonKhidirov <ankhid98@gmail.com>
2024-08-11 10:42:56 +0500

    Icon replaced by mui icon

--- src/component/common/profile-btn/profile-btn.tsx
+++ src/component/common/profile-btn/profile-btn.tsx
@@ -1,21 +1,25 @@
 import type { FC } from 'react'
-import type { TRole } from '@type/user'
+import type { TUserRole } from '@type/user'
 
 import Link from 'next/link'
-import { ProfileIcon } from '@assets/icons'
+import { AccountCircle } from '@mui/icons-material'
 
 import { PROFILE_PAGE } from '@constant/links'
+import { COLORS } from '@constant/colors'
 import styles from './profile-btn.module.css'
 
 type TProfileButton = {
     name: string
-    role: TRole
+    role: TUserRole
 }
 
 const ProfileButton: FC<TProfileButton> = ({ name, role }) => {
     return (
         <Link href={PROFILE_PAGE} title='Profile' className={styles.profile_btn}>
-            <ProfileIcon className={styles.profile_icon} />
+            <AccountCircle
+                className={styles.profile_icon}
+                style={{ width: '100%', height: '100%', fill: COLORS.secondary }}
+            />
             <div className={styles.user_name}>{name}</div>
             <div className={styles.user_role}>{role}</div>
         </Link>
