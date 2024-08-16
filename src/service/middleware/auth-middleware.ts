d4ba12dacb541d0535dec14178d4b23ac0d4ff9a
AnushervonKhidirov <ankhid98@gmail.com>
2024-08-16 20:17:59 +0500

    auth middleware done

--- src/service/middleware/auth-middleware.ts
+++ src/service/middleware/auth-middleware.ts
@@ -5,19 +5,20 @@ import { cookies } from 'next/headers'
 
 import { getUserWithRefresh } from '@api/auth-api'
 import { USER_HEADER } from '@constant/header'
-import { SIGN_IN_PAGE } from '@constant/links'
+import { SIGN_IN_PAGE, JOIN_PAGE } from '@constant/links'
 import { ACCESS_TOKEN, REFRESH_TOKEN } from '@constant/auth'
 
 export function authMiddleware(nextMiddleware: NextMiddleware): NextMiddleware {
     return async (request, event) => {
         const headers = new Headers(request.headers)
+        const pathname = request.nextUrl.pathname
         const cookieStore = cookies()
+
         const accessToken = cookieStore.get(ACCESS_TOKEN)?.value
         const refreshToken = cookieStore.get(REFRESH_TOKEN)?.value
+        const unAuthPage = pathname === SIGN_IN_PAGE || pathname === JOIN_PAGE
 
-        // TODO: skip on sign in and join pages!
-
-        if (accessToken && refreshToken) {
+        if (accessToken && refreshToken && !unAuthPage) {
             const [user, err] = await getUserWithRefresh({
                 access_token: accessToken,
                 refresh_token: refreshToken,
